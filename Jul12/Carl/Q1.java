c07ce7464fc23f5561a2ce0282671169d724de2e
Carl <shikang.liu@mail.mcgill.ca>
2018-07-26 19:17:13 +0800

    save

---
+++ Jul12/Carl/Q1.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+public class Q1{
+	public static void main(String[] args){
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = head;
+	}
+
+	public boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head.next;
+        while(true){
+        	if(slowPointer == fastPointer){
+        		return true;
+        	}
+        	if(slowPointer.next!=null&&fastPointer.next!=null&&fastPointer.next.next!=null){
+        		slowPointer = slowPointer.next;
+        		fastPointer = fastPointer.next.next;
+        	} else {
+        		return false; 
+        	}
+        }
+    }
+}
